1b61e87075b5a184ad390ebf6c03f0d7481a8bc7
SashaOrlova <sasha.orlova.1998n@mail.ru>
2018-05-04 09:27:43 +0300

    change ports

--- ServerClient/src/test/java/ServerClientTest.java
+++ ServerClient/src/test/java/ServerClientTest.java
@@ -7,16 +7,12 @@ import java.io.FileInputStream;
 import static org.junit.Assert.*;
 
 public class ServerClientTest {
-    public class ServerTask implements Runnable {
-        public void run() {
-            Server server = new Server();
-            server.run(3355);
-        }
-    }
-
     @Test
     public void smokeTest() throws Exception {
-        Thread serverThread = new Thread(new ServerTask());
+        Thread serverThread = new Thread(() -> {
+            Server server = new Server();
+            server.run(3355);
+        });
         serverThread.start();
         Thread.sleep(100);
         Client client = new Client();
@@ -27,11 +23,14 @@ public class ServerClientTest {
 
     @Test
     public void getDirectoryContent() throws Exception {
-        Thread serverThread = new Thread(new ServerTask());
+        Thread serverThread = new Thread(() -> {
+            Server server = new Server();
+            server.run(3354);
+        });
         serverThread.start();
         Client client = new Client();
         Thread.sleep(100);
-        client.start(3355);
+        client.start(3354);
         Client.Response response = client.makeRequest("1 src/main/resources/TestExamples");
         assertEquals(2, response.getSize());
         assertTrue((response.getList().get(0).first.equals("a.txt") && response.getList().get(1).first.equals("b.txt"))
@@ -44,11 +43,14 @@ public class ServerClientTest {
 
     @Test
     public void getDirectoryWithDirectory() throws Exception {
-        Thread serverThread = new Thread(new ServerTask());
+        Thread serverThread = new Thread(() -> {
+            Server server = new Server();
+            server.run(3353);
+        });
         serverThread.start();
         Client client = new Client();
         Thread.sleep(100);
-        client.start(3355);
+        client.start(3353);
         Client.Response response = client.makeRequest("1 src/main/resources/DirectoryWithDirectory");
         assertEquals(3, response.getSize());
         boolean wasDir = false, wasC = false, wasFile = false;
@@ -73,11 +75,14 @@ public class ServerClientTest {
 
     @Test
     public void getFile() throws Exception {
-        Thread serverThread = new Thread(new ServerTask());
+        Thread serverThread = new Thread(() -> {
+            Server server = new Server();
+            server.run(3352);
+        });
         serverThread.start();
         Client client = new Client();
         Thread.sleep(100);
-        client.start(3355);
+        client.start(3352);
         Client.Response response = client.makeRequest("2 src/main/resources/TestExamples/a.txt");
         assertEquals(5, response.getSize());
         FileInputStream in = new FileInputStream(response.getFile());
